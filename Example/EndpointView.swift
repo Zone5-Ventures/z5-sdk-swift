9364c8e274d1a2a26df9e43df7117275743aeddf
Daniel Farrelly <daniel@jellystyle.com>
2019-11-08 17:42:38 +1100

    Improve ability to navigate and view endpoint responses

--- Example/EndpointView.swift
+++ Example/EndpointView.swift
@@ -31,42 +31,16 @@ struct EndpointView<Response>: View {
 
 	@State private var displayingError = false
 
-	@State private var properties: [PropertyRepresentation] = []
-
-	struct ObjectRepresentation: Identifiable {
-
-		var id = UUID()
-
-		var properties: [PropertyRepresentation]
-
-	}
-
-	struct PropertyRepresentation: Identifiable {
-
-		var id = UUID()
-
-		var label: String
-
-		var value: Text
-
-	}
-
 	init(title: String, apiClient: Zone5 = .shared, handler: @escaping Handler) {
 		self.title = title
 		self.apiClient = apiClient
 		self.handler = handler
 	}
 
+	@State private var list = ObjectView(object: nil)
+
 	var body: some View {
-		List(properties, rowContent: { property in
-			VStack(alignment: .leading, spacing: 5) {
-				Text(property.label)
-					.font(.footnote)
-					.foregroundColor(.secondary)
-				property.value
-					.font(.system(.body, design: .monospaced))
-			}
-		})
+		return list
 		.onAppear {
 			self.performHandler()
 		}
@@ -84,99 +58,20 @@ struct EndpointView<Response>: View {
 	}
 
 	private func performHandler() -> Void {
-		self.error = nil
-		self.isLoading = true
-		self.handler(self.apiClient, self.handlerDidComplete)
+		error = nil
+		isLoading = true
+		handler(apiClient, handlerDidComplete)
 	}
 
 	private func handlerDidComplete(_ result: Result<Response, Zone5.Error>) -> Void {
-		defer { self.isLoading = false }
+		defer { isLoading = false }
 
 		switch result {
 		case .success(let response):
-			let mirrors: [Mirror]
-			if let array = response as? Array<Any> {
-				mirrors = array.map { Mirror(reflecting: $0) }
-			}
-			else {
-				mirrors = [Mirror(reflecting: response)]
-			}
+			list = ObjectView(object: response)
 
-			self.properties = mirrors.flatMap { $0.children }.compactMap { child in
-				print(child)
-
-				guard let label = child.label else {
-					return nil
-				}
-
-				let value: Text
-
-				if let optional = child.value as? OptionalProtocol {
-					if optional.containsValue {
-						value = displayValue(for: optional.wrappedValue)
-					}
-					else {
-						value = Text("nil").foregroundColor(.secondary).italic()
-					}
-				}
-				else {
-					value = displayValue(for: child.value)
-				}
-
-				return PropertyRepresentation(label: label, value: value)
-			}
-
-		case .failure(let error):
-			self.error = error
-		}
-	}
-
-	private func displayValue(for value: Any) -> Text {
-		let type = Swift.type(of: value)
-		var foregroundColor: Color?
-
-		if type == String.self {
-			foregroundColor = .black
-		}
-		else if type == Int.self || type == Int8.self || type == Int16.self || type == Int32.self || type == Int64.self || type == UInt.self || type == UInt8.self || type == UInt16.self || type == UInt32.self || type == UInt64.self || type == Double.self || type == Float.self {
-			foregroundColor = .blue
-		}
-		else {
-			foregroundColor = .green
-		}
-
-		var displayValue = Text(String(describing: value))
-
-		if let foregroundColor = foregroundColor {
-			displayValue = displayValue.foregroundColor(foregroundColor)
-		}
-
-		return displayValue
-	}
-
-}
-
-protocol OptionalProtocol {
-
-	var containsValue: Bool { get }
-
-	var wrappedValue: Any { get }
-
-}
-
-extension Optional: OptionalProtocol {
-
-	var containsValue: Bool {
-		switch self {
-		case .some(_): return true
-		case .none: return false
-		}
-	}
-
-	var wrappedValue: Any {
-		switch self {
-		case .some(let value): return value
-		case .none: fatalError()
+		case .failure(let response):
+			error = response
 		}
 	}
 
---
+++ Example/ObjectView.swift
@@ -0,0 +1,177 @@
+//
+//  ObjectView.swift
+//  Zone5 Example
+//
+//  Created by Daniel Farrelly on 25/10/19.
+//  Copyright © 2019 Zone5 Ventures. All rights reserved.
+//
+
+import Foundation
+
+import SwiftUI
+import Zone5
+
+struct ObjectView: View {
+
+	let object: Any?
+
+	init(object: Any?) {
+		self.object = object
+	}
+
+	struct PropertyRepresentation: Identifiable {
+
+		var id = UUID()
+
+		var label: String
+
+		var value: Text
+
+		var object: Any?
+
+	}
+
+	private var properties: [PropertyRepresentation] {
+		var properties: [PropertyRepresentation] = []
+
+		if let array = object as? Array<Any> {
+			for (i, element) in array.enumerated() {
+				properties.append(property(for: element, label: "Index \(i)"))
+			}
+		}
+		else if let object = object {
+			for child in Mirror(reflecting: object).children {
+				guard let label = child.label else {
+					continue
+				}
+
+				properties.append(property(for: child.value, label: label))
+			}
+		}
+
+		return properties
+	}
+
+	var body: some View {
+		List {
+			ForEach(properties) { property -> AnyView in
+				let stack = VStack(alignment: .leading, spacing: 5) {
+					Text(property.label)
+						.font(.footnote)
+						.foregroundColor(.secondary)
+						.lineLimit(1)
+					property.value
+						.font(.system(.body, design: .monospaced))
+						.lineLimit(1)
+				}
+
+				if let object = property.object {
+					let objectView = ObjectView(object: object)
+						.navigationBarTitle("\(property.label)", displayMode: .inline)
+
+					return AnyView(NavigationLink(destination: objectView) {
+						return stack
+					})
+				}
+				else {
+					return AnyView(stack)
+				}
+			}
+		}
+		.listStyle(GroupedListStyle())
+	}
+
+	private func property(for value: Any, label: String) -> PropertyRepresentation {
+		if let optional = value as? OptionalProtocol {
+			if optional.containsValue {
+				return property(for: optional.wrappedValue, label: label)
+			}
+			else {
+				let value = Text("nil").foregroundColor(.secondary).italic()
+				return PropertyRepresentation(label: label, value: value, object: nil)
+			}
+		}
+		else {
+			let mirror = Mirror(reflecting: value)
+			var objectForNavigation: Any?
+			let displayValue: Text
+
+			if case .enum = mirror.displayStyle {
+				displayValue = Text(String(describing: value))
+					.foregroundColor(.purple)
+			}
+			else if mirror.subjectType == String.self {
+				displayValue = Text(String(describing: value))
+			}
+			else if mirror.subjectType == Int.self || mirror.subjectType == Int8.self || mirror.subjectType == Int16.self || mirror.subjectType == Int32.self || mirror.subjectType == Int64.self || mirror.subjectType == UInt.self || mirror.subjectType == UInt8.self || mirror.subjectType == UInt16.self || mirror.subjectType == UInt32.self || mirror.subjectType == UInt64.self || mirror.subjectType == Double.self || mirror.subjectType == Float.self {
+				displayValue = Text(String(describing: value))
+					.foregroundColor(.blue)
+			}
+			else {
+				objectForNavigation = value
+				displayValue = Text(String(describing: value))
+					.foregroundColor(.green)
+			}
+
+			return PropertyRepresentation(label: label, value: displayValue, object: objectForNavigation)
+		}
+	}
+
+}
+
+private protocol OptionalProtocol {
+
+	var containsValue: Bool { get }
+
+	var wrappedValue: Any { get }
+
+}
+
+extension Optional: OptionalProtocol {
+
+	var containsValue: Bool {
+		switch self {
+		case .some(_): return true
+		case .none: return false
+		}
+	}
+
+	var wrappedValue: Any {
+		switch self {
+		case .some(let value): return value
+		case .none: fatalError()
+		}
+	}
+
+}
+
+struct ObjectView_Previews: PreviewProvider {
+    static var previews: some View {
+		Group {
+			NavigationView { () -> ObjectView in
+				var user = User()
+				user.id = 12345
+				user.email = "jane.smith@example.com"
+				user.firstName = "Jane"
+				user.lastName = "Smith"
+
+				return ObjectView(object: user)
+			}
+			NavigationView { () -> ObjectView in
+				var user1 = User()
+				user1.id = 12345
+				user1.email = "jane.smith@example.com"
+				user1.firstName = "Jane"
+				user1.lastName = "Smith"
+
+				var user2 = User()
+				user2.id = 67890
+				user2.email = "john.smith@example.com"
+				user2.firstName = "John"
+				user2.lastName = "Smith"
+
+				return ObjectView(object: [user1, user2])
+			}
+		}
+    }
+}
