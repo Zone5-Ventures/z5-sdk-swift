cac4e47232e3458abd28557a780664f26d7cdd87
Daniel Farrelly <daniel@jellystyle.com>
2019-11-14 14:22:15 +1100

    Enable access to Development Assets from SPM unit tests

--- Zone5Tests/Utilities/Bundle.swift
+++ Zone5Tests/Utilities/Bundle.swift
@@ -18,6 +18,19 @@ extension Bundle {
 
 	// MARK: Development assets
 
+	/// URL for the project source directory, used as a basis for accessing development assets from tests when run using
+	/// Swift Package Manager, which doesn't have support for bundled resources at the time of writing.
+	///
+	/// The value is determined by creating a URL based on the path for this source file, and then manually walking up
+	/// to the root directory for the project.
+	private func developmentAssetsURL(with subdirectory: String?) -> URL {
+		return URL(fileURLWithPath: #file)
+			.deletingLastPathComponent() // Remove `Bundle.swift`
+			.deletingLastPathComponent() // Remove `Utilities`
+			.deletingLastPathComponent() // Remove `Zone5Tests`
+			.appendingPathComponent(developmentAssetsDirectory(with: subdirectory))
+	}
+
 	private func developmentAssetsDirectory(with subdirectory: String?) -> String {
 		var developmentAssetsDirectory = "Development Assets"
 
@@ -28,19 +41,26 @@ extension Bundle {
 		return developmentAssetsDirectory
 	}
 
+	private func contents(of directoryURL: URL) -> [URL]? {
+		return try? FileManager.default.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil, options: [])
+	}
+
 	func url(forDevelopmentAsset name: String, withExtension ext: String, subdirectory: String? = nil) -> URL? {
-		let subdirectory = developmentAssetsDirectory(with: subdirectory)
-		return url(forResource: name, withExtension: ext, subdirectory: subdirectory)
+		let developmentAssetsSubdirectory = developmentAssetsDirectory(with: subdirectory)
+		return url(forResource: name, withExtension: ext, subdirectory: developmentAssetsSubdirectory)
+			?? developmentAssetsURL(with: subdirectory).appendingPathComponent("\(name).\(ext)")
 	}
 
 	func urls(forDevelopmentAssetsWithExtension ext: String, subdirectory: String? = nil) -> [URL]? {
-		let subdirectory = developmentAssetsDirectory(with: subdirectory)
-		return urls(forResourcesWithExtension: ext, subdirectory: subdirectory)
+		let developmentAssetsSubdirectory = developmentAssetsDirectory(with: subdirectory)
+		return urls(forResourcesWithExtension: ext, subdirectory: developmentAssetsSubdirectory)
+			?? contents(of: developmentAssetsURL(with: subdirectory))?.filter { $0.pathExtension == ext }
 	}
 
 	func urlsForDevelopmentAssets(subdirectory: String? = nil) -> [URL]? {
-		let subdirectory = developmentAssetsDirectory(with: subdirectory)
-		return urls(forResourcesWithExtension: nil, subdirectory: subdirectory)
+		let developmentAssetsSubdirectory = developmentAssetsDirectory(with: subdirectory)
+		return urls(forResourcesWithExtension: nil, subdirectory: developmentAssetsSubdirectory)
+			?? contents(of: developmentAssetsURL(with: subdirectory))
 	}
 
 }
