069f80554aed49687160c8c8d1068c46377cfaa4
John Keller <mail@johnkeller101.com>
2020-12-04 09:29:34 -0700

    make decorated log only happen in debug

--- Zone5/Transport/URLRequestInterceptor.swift
+++ Zone5/Transport/URLRequestInterceptor.swift
@@ -88,7 +88,9 @@ internal class URLRequestInterceptor: URLProtocol {
 	private func decorateAndSendRequest() {
 		// decorate headers. This needs to be after the refresh code as the token may change
 		let request = URLRequestInterceptor.decorate(request: self.request)
+        #if DEBUG
         print("Decorated for \(request.url?.absoluteString ?? ""): \(request.allHTTPHeaderFields ?? [:])")
+        #endif
 		// send
 		sendRequest(request)
 	}
