7595274b8c70f4f0173317ba1acb9bb2de85de3c
Jean Hall <jean@todaysplan.com.au>
2021-01-27 16:51:58 +1100

    don't set start time on upload context. it messes with accumulation.

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -283,7 +283,6 @@ struct ContentView: View {
 						var context = DataFileUploadContext()
 						context.equipment = .gravel
 						context.name = "Epic Ride"
-						context.startTime = .now
 						//context.bikeID = "d584c5cb-e81f-4fbe-bc0d-667e9bcd2c4c"
 
 						client.activities.upload(fileURL, context: context) { result in
